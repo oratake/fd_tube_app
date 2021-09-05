535f340715506bdd6430bf3ce4fb55aba6f292ab
kenichiro tsunoda <kenichirotsunoda@kenichironoMacBook-Air.local>
2021-09-05 14:45:25 +0900

    ヘルパーメソッドをコメントアウト

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  # def aws_s3
+  #   s3 = get_s3_resource
+  #  end
+   
+  #  private
+
+  #  def get_s3_resource
+  #    region = <バケットのリージョン>
+  #    Aws::S3::Resource.new(
+  #      region: region,
+  #      credentials: Aws::Credentials.new(
+  #          ENV['AWS_ACCESS_KEY'],
+  #          ENV['AWS_SECRET_KEY']
+  #      )
+  #    )
+  #  end
 end
