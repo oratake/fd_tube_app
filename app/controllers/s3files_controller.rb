b331684893933b9673789891de9a43810454ddd7
kenichiro tsunoda <kenichirotsunoda@kenichironoMacBook-Air.local>
2021-09-07 14:07:45 +0900

    Gemfileにデバック追加、S3fileへのcreate動作確認

--- app/controllers/s3files_controller.rb
+++ app/controllers/s3files_controller.rb
@@ -2,7 +2,7 @@ class S3filesController < ApplicationController
   def initialize
     super
     @region = 'ap-northeast-1'
-    @bucketname = ENV['AWS_S3_BUCKET_TNAME'] 
+    @bucketname = ENV['AWS_S3_BUCKET_NAME'] 
     @s3 = get_s3_resource
   end
 
@@ -18,21 +18,21 @@ class S3filesController < ApplicationController
     # ポストされたfileデータを取得
     file = s3file_params[:key]
     filename = file.original_filename
-  
+    
     #  一時保存用のパスにファイルを保存 
     file_path = "tmp/s3/#{filename}"
     File.binwrite(file_path, file.read)
-  
+    
     # バケット名を指定
     bucket = @s3.bucket(@bucketname)
-  
+    
     # バケットに保存する際の一意の識別名（ファイル名）を指定 
     key = filename
     # S3にアップロードする際に特定のディレクトリに入れたい場合は、filename の先頭に
     # ディレクトリ名を追加してください
     # 例：key = "dir1/dir2/#{filename}"）
     object = bucket.object(key)
-  
+
     # upload_fileメソッドを使って、S3上のバケットにファイルをアップロードする 
     # 　第一引数 = 一時保存してあるファイルのパス 
     # 　第二引数 = オプション(aclはアクセス権) 
@@ -67,5 +67,9 @@ class S3filesController < ApplicationController
           ENV['AWS_SECRET_KEY']
        )
     )
+  end
+
+  def s3file_params
+    params.require(:s3file).permit(:key)
   end
 end
