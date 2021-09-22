9d5f2ad82784e577bf078df74b63dfe8f7431148
kenichiro tsunoda <kenichirotsunoda@kenichironoMacBook-Air.local>
2021-09-22 09:28:12 +0900

    管理者としてログイン→管理者用ページ遷移を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    videos_path(resource)
+    if current_user.admin?
+      admin_videos_path
+    else
+      videos_path
+    end
   end
 
   def configure_permitted_parameters
