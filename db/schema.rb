582afc2a443fd11d5fa4d361aa1e17b5a512bfba
kenichiro tsunoda <kenichirotsunoda@kenichironoMacBook-Air.local>
2021-09-27 06:46:25 +0900

    Userテーブルにユーザー名,氏名,名前を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_145007) do
+ActiveRecord::Schema.define(version: 2021_09_26_214319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_09_21_145007) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "video_id"
+    t.string "file_name"
     t.index ["video_id"], name: "index_s3files_on_video_id"
   end
 
@@ -31,8 +32,10 @@ ActiveRecord::Schema.define(version: 2021_09_21_145007) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "user_name"
     t.boolean "admin", default: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
