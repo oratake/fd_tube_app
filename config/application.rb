693690ce06029252728298c80137aad7b55e7be8
kenichiro tsunoda <kenichirotsunoda@kenichironoMacBook-Air.local>
2021-09-27 22:28:04 +0900

    railsの時刻設定を日本基準に変更

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Fire
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
-
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -19,5 +19,7 @@ module Fire
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.time_zone = 'Tokyo'
   end
 end
