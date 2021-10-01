b80b62a7215d94f5055097ccffb62c7bc15aed91
kenichiro tsunoda <kenichirotsunoda@kenichironoMacBook-Air.local>
2021-10-01 13:08:21 +0900

    ユーザー情報編入機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: "home#top"
+  # root to: "home#top"
+  root to: "videos#index"
   devise_for :users, :controllers => {
     :registrations => 'users/registrations'
    }
