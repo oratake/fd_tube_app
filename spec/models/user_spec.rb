f82cdd436b665f39bb6587d0fcb6d46329144e9a
Kenichiro <kenkennfree@gmail.com>
2021-11-15 23:33:59 +0900

    modelsスペック追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   describe "Userモデル" do
-    it "必須項目がnilの無効な状態であること" do
-      user = User.new(
-        user_name: "　　",
-        email: "john@example.com",
-        encrypted_password: "john@example.com",
-        )
-       expect(user).to_not be_nil
+    it "バリデーションが発生していないこと" do
+      user = build(:user)
+      expect(user).to be_valid
     end
   end
 end
---
+++ spec/models/video_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Video, type: :model do
+  describe "Videoモデル" do
+    it "バリデーションが発生していないこと" do
+      video = build(:video)
+      expect(video).to be_valid
+    end
+  end
+end
