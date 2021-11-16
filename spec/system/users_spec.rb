66fe9d94a4fc1b3b0fca1ef3a7415707dc55149e
Kenichiro <kenkennfree@gmail.com>
2021-11-16 09:27:44 +0900

    RSpecサポートファイル追加

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.descirbe User, type: :system do 
+  # ログイン前
+    # フォームが正常な場合新規登録できること
+    # 必須項目が未入力の場合に登録できないこと
+    # email がダブっている時に登録できないこと
+  
+  # ログイン後
+    
+end
