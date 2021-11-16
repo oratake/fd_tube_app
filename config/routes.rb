493fa6977b993045c5795de1be36d4f32646aed8
Kenichiro <kenkennfree@gmail.com>
2021-11-16 10:11:47 +0900

    ルートパス修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root to: "home#top"
+  # root to: "home#top"
+  devise_scope :user do
+    root "users/sessions#new"
+  end
+  
   devise_for :users, :controllers => {
     :registrations => 'users/registrations'
    }
