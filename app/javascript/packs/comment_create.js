eb9238ceaf41dfe75ea06df2794cbaaf36e19ae1
Kenichiro <kenkennfree@gmail.com>
2021-11-25 16:39:12 +0900

    axiosのパスを修正

--- app/javascript/packs/comment_create.js
+++ app/javascript/packs/comment_create.js
@@ -14,10 +14,7 @@ new Vue({
     methods: {
         comment_create: function() {
             axios
-                // 本番環境
-                .post('http://ecs-fd-tube-app-alb-242793914.ap-northeast-1.elb.amazonaws.com/api/v1/comments.json', {
-                // ローカル
-                // .post('http://localhost:80/api/v1/comments.json', {
+                .post('/api/v1/comments.json', {
                     comment: {
                         context: this.context,
                         user_id: this.user_id,
